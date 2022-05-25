be48fcb0d29be4483cb5db561a3bf6e262bbad9f
ZeljkoMutic <Zeljkomtc2@gmail.com>
2022-05-25 12:05:06 +0200

    Learned functions, how to create project with cargo, how to make an executable, and how to test the optimization of the code

---
+++ hello_cargo/target/debug/deps/hello_cargo-c5ee3f8973577d48.d
@@ -0,0 +1,5 @@
+C:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\debug\deps\hello_cargo-c5ee3f8973577d48.rmeta: src\main.rs
+
+C:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\debug\deps\hello_cargo-c5ee3f8973577d48.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-c5ee3f8973577d48.d
@@ -0,0 +1,5 @@
+c:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\rls\debug\deps\hello_cargo-c5ee3f8973577d48.rmeta: src\main.rs
+
+c:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\rls\debug\deps\hello_cargo-c5ee3f8973577d48.d: src\main.rs
+
+src\main.rs:
