d3900aa12bec17ac69adbc7e69bb86987e7001d9
ZeljkoMutic <Zeljkomtc2@gmail.com>
2022-05-25 11:57:14 +0200

    Learned how to make functions in Rust, how to create projects, and how to build them!

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\zmutic\Desktop\LearningRust\hello_cargo\src\main.rs
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-80b5d0f509114918.d
@@ -0,0 +1,5 @@
+c:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.rmeta: src\main.rs
+
+c:\Users\zmutic\Desktop\LearningRust\hello_cargo\target\rls\debug\deps\hello_cargo-80b5d0f509114918.d: src\main.rs
+
+src\main.rs:
