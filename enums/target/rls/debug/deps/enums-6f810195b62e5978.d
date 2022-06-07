8ff16e7adea403306e66f89f64cdf398202e7027
ZeljkoMutic <Zeljkomtc2@gmail.com>
2022-06-07 10:19:38 +0200

    Enums

---
+++ enums/target/rls/debug/deps/enums-6f810195b62e5978.d
@@ -0,0 +1,5 @@
+c:\Users\zmutic\Desktop\Learning-Rust\enums\target\rls\debug\deps\enums-6f810195b62e5978.rmeta: src\main.rs
+
+c:\Users\zmutic\Desktop\Learning-Rust\enums\target\rls\debug\deps\enums-6f810195b62e5978.d: src\main.rs
+
+src\main.rs:
---
+++ enums/target/rls/debug/deps/enums-c3365cae0d8a5cd4.d
@@ -0,0 +1,5 @@
+c:\Users\zmutic\Desktop\Learning-Rust\enums\target\rls\debug\deps\enums-c3365cae0d8a5cd4.rmeta: src\main.rs
+
+c:\Users\zmutic\Desktop\Learning-Rust\enums\target\rls\debug\deps\enums-c3365cae0d8a5cd4.d: src\main.rs
+
+src\main.rs:
